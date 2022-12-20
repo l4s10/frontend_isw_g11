3a4b6d6b9cf717b9d27f4de873b7f04a749fe083
Main PC <franciscomunoz142@gmail.com>
2022-12-20 16:07:07 -0300

    Cambiando cartas mantenciones

--- src/Components/GenericMantentionCard.js
+++ src/Components/GenericMantentionCard.js
@@ -2,20 +2,20 @@ import { Grid, Button, Card, CardContent } from "@mui/material";
 import { LoremIpsum } from "react-lorem-ipsum";
 import './Styleforpages.css';
 
-export default function MantentionCard(){
+export default function GenericMantentionCard(props){
     return(
         <div className="div-padre">
             <Card>
                 <CardContent>
                 <Grid >
-                <h1 className="titulo">Mantención N°1</h1>
+                <h1 className="titulo">{props.name}</h1>
                 <div>
                     <p className="descripcion">
-                        <LoremIpsum p={2}></LoremIpsum>
+                        {props.descripcion}
                     </p>
                     <Grid>
-                            <img src="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg" alt="IMG ANTES" className="fotos"></img>
-                            <img src="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg" alt="IMG DESPUES" className="fotos"></img>
+                            <img src={props.urlBefore} alt="IMG ANTES" className="fotos"></img>
+                            <img src={props.urlAfter} alt="IMG DESPUES" className="fotos"></img>
                     </Grid>
                     <Button className="botonInforme" size="large" variant="contained">Descargar informe</Button>
                 </div>
--- src/Components/MantentionsPage.js
+++ src/Components/MantentionsPage.js
@@ -1,6 +1,8 @@
 //Imports
 import ButtonAppBar from "./Examplenav";
 import MantencionCard from "./MantencionCard";
+import GenericMantentionCard from "./GenericMantentionCard";
+import { LoremIpsum } from "react-lorem-ipsum";
 import './Styleforpages.css';
 
 export default function Mantentions(){
@@ -9,10 +11,26 @@ export default function Mantentions(){
             <div>
                 <ButtonAppBar></ButtonAppBar>
             </div>
-            <div>
-                <h1 className="titulo">Mural de mantenciones</h1>
-                <MantencionCard/>
-                <MantencionCard/>
+            <h1 className="titulo">Mural de mantenciones</h1>
+            <div className="contenedorCards">
+                <GenericMantentionCard
+                    name="Mantención N°1"
+                    descripcion={<LoremIpsum p={1}/>}
+                    urlBefore="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg"
+                    urlAfter="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg"
+                />
+                <GenericMantentionCard
+                    name="Mantención N°2"
+                    descripcion={<LoremIpsum p={2}/>}
+                    urlBefore="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg"
+                    urlAfter="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg"
+                />
+                <GenericMantentionCard
+                    name="Mantención N°3"
+                    descripcion={<LoremIpsum p={3}/>}
+                    urlBefore="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg"
+                    urlAfter="https://cdn2.hubspot.net/hubfs/6430607/01%20-%20Blog/Reja%20de%20acero%20gris%20blog.jpg"
+                />
             </div>
         </div>
     );
